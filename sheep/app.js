4b9a9c6044fe01f6df42960defea04131b14d6b7
janeshin059 <janeshin059@gmail.com>
2021-04-30 12:14:02 +0900

    Sheeps

--- sheep/app.js
+++ sheep/app.js
@@ -1,9 +1,18 @@
+import { Hill } from "./hill.js";
+
+import {SheepController} from "./sheep-controller.js"
 class App {
   constructor() {
     this.canvas = document.createElement('canvas');
     this.ctx = this.canvas.getContext("2d");
     document.body.appendChild(this.canvas);
 
+    this.hills = [
+      new Hill('#fd6bea', 0.2, 12),
+      new Hill('#ff59c2', 0.5,8),
+      new Hill('#ff4674',1.4,6)
+    ]
+    this.SheepController = new SheepController();
     window.addEventListener('resize', this.resize.bind(this), false);
     this.resize();
     requestAnimationFrame(this.animate.bind(this));
@@ -16,13 +25,25 @@ class App {
     this.canvas.width = this.stageWidth * 2;
     this.canvas.height = this.stageHeight * 2;
     this.ctx.scale(2,2);
+
+    for(let i = 0; i<this.hills.length; i++){
+      this.hills[i].resize(this.stageWidth, this.stageHeight);
+    }
+    this.SheepController.resize(this.stageWidth, this.stageHeight);
   }
+
 
   animate(t) {
     requestAnimationFrame(this.animate.bind(this));
-    //canvs를 깨끗하게 씌워 주기
+    //canvas를 깨끗하게 씌워 주기
     this.ctx.clearRect(0,0, this.stageWidth, this.stageHeight);
+    let dots;
+    for(let i = 0; i<this.hills.length; i++){
+      dots = this.hills[i].draw(this.ctx);
+    }
+    this.SheepController.draw(this.ctx,t,dots);
   }
+  
 }
 
 window.onload = () => {
